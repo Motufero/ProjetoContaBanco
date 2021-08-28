7f437e403ca051836d11545ec120191a6faafa5b
Saulo Gabriel da Costa Ferreira <37157092+Motufero@users.noreply.github.com>
2021-08-28 15:29:14 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -166,7 +166,7 @@ void iniciar (){
 }
 
 int main(){
-    cout<<"\n_____________________SEJA_BEM_VINDO!_______________________"<<endl;
+    cout<<"\n_____________________SEJA_BEM_VINDO!________________________"<<endl;
 
     iniciar();
     return 0;
